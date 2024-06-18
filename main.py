d343936f258fbf011d360ee9753e782bd98d278e
Luke Finch <lfinch@joriehc.com>
2024-06-17 22:48:06 -0500

    #3 done

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # 1) Read the new_or_established.csv file. Pass as parameter one name present in the spreadsheet. 
-# If that name has visited the same location within 3 years and their status is not "canceled", 
+# If that name has visited the same location within 3 years and their status is not "cancelled", 
 # return the string "Established". Else, return "New Patient"
 class csvHandling:
 
@@ -24,14 +24,24 @@ class csvHandling:
         Parameter 2: any patient name from the file
         Return: String "Established" or "New Patient"
         """
+        patientDF = df[df['name'] == patientName]
+
         established = False
-        for index, row in df.iterrows():
-            while row['name'] == patientName:
-                currentLoc = row['location']
-                while row['location'] == currentLoc:
-                    if int(row['date'][6:]) <= 3 and row['status'] != "cancelled":
-                        established = True
-                        break
+        
+        for index, row in patientDF.iterrows(): # Loops through each row
+            currentLoc = row['location']
+            nextLoc = row['location'][index + 1]
+
+            currentDate = int(row['date'][6:])
+            earliestDate = int(row['date'][6:])
+
+            for index, row in patientDF[currentLoc].iterrows(): # Loops through each row with same location
+                if currentLoc != nextLoc:
+                    earliestDate = int(row['date'][6:])
+
+            if currentDate - earliestDate <= 3 and row['status'] != "cancelled":
+                established = True
+                break
 
         if established == True:
             return "Established"
@@ -52,11 +62,16 @@ class OneHealthCareLogin:
     def providerSignIn(driver, email, password, phoneNumber, businessName, streetAddress, city, state, zipCode, taxIDNum, taxProvider):
         
         ### Signing in
-
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(text(),'Sign in')]")))
-        driver.save_screenshot('screenshot_before_timeout.png')
-        pyautogui.scroll(-50)
-        driver.save_screenshot('screenshot_after_scroll.png')
+        
+        # Scrolling down so computer can see full "Sign in" element
+        pyautogui.moveTo(1900, 500) # Move mouse to scroll bar
+        pyautogui.mouseDown()
+        pyautogui.moveTo(1900, 800, duration=0.5)
+        pyautogui.mouseUp
+        
+        # WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(text(),'Sign in')]")))
+        # driver.save_screenshot('screenshot_before_timeout.png')
+        # driver.save_screenshot('screenshot_after_scroll.png')
         driver.find_element(By.XPATH, "//button[contains(text(),'Sign in')]").click()
 
         # Entering login credentials
@@ -98,43 +113,64 @@ class OutlookHandling:
 
     def openOutlookBrowser(emailAddress):
         # Opening outlook on a browser
+        edge_browser = pyautogui.locateCenterOnScreen(r"C:\Users\LukeFinch\OneDrive - Jorie Healthcare\Pictures\Screenshots\Screenshot 2024-06-17 222915.png", confidence=0.9)
+        pyautogui.moveTo(edge_browser)
+        pyautogui.click()
+        
         pyautogui.hotkey("ctrl", "t")
-        sleep(1)
+        pyautogui.sleep(1)
         pyautogui.write("outlook.com")
         pyautogui.hotkey("enter")
-        sleep(1)
+        pyautogui.sleep(4)
 
         # Signing in
-        sign_in_button = pyautogui.locateCenterOnScreen("outlook_signin_icon.png", confidence=0.9)
-        pyautogui.moveTo(sign_in_button, 1)
+        sign_in_button = pyautogui.locateCenterOnScreen(r"C:\Users\LukeFinch\OneDrive - Jorie Healthcare\Pictures\Screenshots\outlook_signin_icon.png", confidence=0.9)
+        pyautogui.moveTo(sign_in_button)
         pyautogui.click()
-        pyautogui.hotkey("enter")
 
+        pyautogui.sleep(2)
+        pyautogui.moveTo(pyautogui.locateCenterOnScreen(r"C:\Users\LukeFinch\OneDrive - Jorie Healthcare\Desktop\snippets\outlook_browser_signin_bar.png", confidence=0.9))
+        pyautogui.click()
         pyautogui.write(emailAddress)
         pyautogui.hotkey("enter")
 
-    
-    def openOutlookApp():
-        # Opening outlook through the app instead of through a browser
-        search_bar = pyautogui.locateCenterOnScreen("windows_searchbar_icon.png", confidence=0.8)
-        pyautogui.moveTo(search_bar, 1)
-        pyautogui.click()
-        pyautogui.write("outlook")
-        pyautogui.hotkey("enter")
-
-
-
-    def openEmail(sender, subject):
+    def openEmailBrowser(sender, subject):
         """
         Purpose: Opening an email based on the subject and the sender by searching in the searchbar
         Parameter 1: subject of email
         Parameter 2: sender of email
         Return: None
         """
-        outlook_searchbar = pyautogui.locateCenterOnScreen("outlook_searchbar.png", confidence=0.9)
-        pyautogui.moveTo(outlook_searchbar, 1)
+        pyautogui.sleep(6)
+        outlook_searchbar = pyautogui.locateCenterOnScreen(r"C:\Users\LukeFinch\OneDrive - Jorie Healthcare\Desktop\snippets\outlook_browser_searchbar.png", confidence=0.7)
+        pyautogui.moveTo(outlook_searchbar)
         pyautogui.click()
-        pyautogui.write(sender + subject)
+        pyautogui.write(sender + " " + subject)
+        pyautogui.hotkey("enter")
+
+    
+    def openOutlookApp():
+        # Opening outlook through the app instead of through a browser
+        search_bar = pyautogui.locateCenterOnScreen(r"C:\Users\LukeFinch\OneDrive - Jorie Healthcare\Desktop\snippets\windows_searchbar_icon.png", confidence=0.8)
+        pyautogui.moveTo(search_bar)
+        pyautogui.click()
+        pyautogui.write("outlook")
+        pyautogui.hotkey("enter")
+
+
+
+    def openEmailApp(sender, subject):
+        """
+        Purpose: Opening an email based on the subject and the sender by searching in the searchbar
+        Parameter 1: subject of email
+        Parameter 2: sender of email
+        Return: None
+        """
+        pyautogui.sleep(2)
+        outlook_searchbar = pyautogui.locateCenterOnScreen(r"C:\Users\LukeFinch\OneDrive - Jorie Healthcare\Desktop\snippets\outlook_searchbar.png", confidence=0.9)
+        pyautogui.moveTo(outlook_searchbar)
+        pyautogui.click()
+        pyautogui.write(sender + " " + subject)
         pyautogui.hotkey("enter")
 
 
@@ -142,18 +178,20 @@ class OutlookHandling:
 def main():
     ### 1
     # df = csvHandling.getDF("new_or_established 1.csv")
-    # for index, row in df.iterrows():
-    #     print(row['location'])
+    # csvHandling.estOrNew(df, "Ana")
 
     
     ### 2
-    driver = OneHealthCareLogin.getDriver("https://provider.umr.com/")
-    OneHealthCareLogin.providerSignIn(driver, "lukefinch45@gmail.com", "Basketball23$", 2243016394, "Luke", "2929 Southwestern Blvd", "Dallas", "TX", 75225, 12345, "JohnDoe")
-
+    # driver = OneHealthCareLogin.getDriver("https://provider.umr.com/")
+    # OneHealthCareLogin.providerSignIn(driver, "lukefinch45@gmail.com", "Basketball23$", 2243016394, "Luke", "2929 Southwestern Blvd", "Dallas", "TX", 75225, 12345, "JohnDoe")
+    
 
     ### 3
-    # OutlookHandling.openOutlookApp()
-    # OutlookHandling.openEmail("Kelly Long", "Compliance Update : Patient Data Security: Upholding Integrity as a HIPAA Priority.")
+    OutlookHandling.openOutlookApp()
+    OutlookHandling.openEmailApp("Kelly Long", "Compliance Update : Patient Data Security: Upholding Integrity as a HIPAA Priority.")
+    
+    # OutlookHandling.openOutlookBrowser("lfinch@joriehc.com")
+    # OutlookHandling.openEmailBrowser("Kelly Long", "Compliance Update : Patient Data Security: Upholding Integrity as a HIPAA Priority.")
 
 
 if __name__ == "__main__":
